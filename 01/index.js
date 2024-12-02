e35a4eb92b398a1e67ee977acad2bea807e63ea8
Fred Marecesche <fred@froddd.co.uk>
2024-12-02 00:59:28 +0000

    Day 01 part 2

--- 01/index.js
+++ 01/index.js
@@ -14,6 +14,18 @@ const part1 = input => {
 
 const part2 = input => {
 
+	const lines = input.map(line => line.split(/\s+/).map(Number))
+	const list1 = lines.map(line => line[0]).sort()
+	const list2 = lines.map(line => line[1]).sort()
+
+	let totalDiff = 0
+	for(let i = 0; i < list1.length; i++) {
+		const instances = list2.filter(x => x === list1[i]).length
+		totalDiff += list1[i] * instances
+	}
+	
+	return totalDiff
+
 }
 
 module.exports = {

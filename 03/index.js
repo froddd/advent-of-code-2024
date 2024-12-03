50ac8c11e3c1624126576a777c38a694e000d5d2
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-12-03 12:22:37 +0000

    Day 03 part 2

--- 03/index.js
+++ 03/index.js
@@ -1,5 +1,5 @@
-const part1 = input => {
-    const matches = input.join('').match(/mul\(\d{1,3},\d{1,3}\)/g)
+const processCode = code => {
+    const matches = code.match(/mul\(\d{1,3},\d{1,3}\)/g)
 
     return matches.reduce((total, mul) => {
         const [a, b] = mul.match(/\d+/g)
@@ -7,6 +7,16 @@ const part1 = input => {
     }, 0)
 }
 
+const part1 = input => {
+    return processCode(input.join(''))
+}
+
+const part2 = input => {
+    const cleanedUp = input.join('').replace(/don't\(\).*?(?=do\(\))/g, '')
+    return processCode(cleanedUp)
+}
+
 module.exports = {
-    part1
+    part1,
+    part2
 }

f7df65df87f468dee25907e85c5c1441a254db22
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-12-03 09:44:17 +0000

    Day 02 part 2

--- 02/index.js
+++ 02/index.js
@@ -1,32 +1,35 @@
+const invalidReportLevel = (levels) => {
+    let reportDirection = 0
+
+    for (let i = 0; i < levels.length -1; i++) {
+        const currentLevel = levels[i]
+        const nextLevel = levels[i+1]
+
+        const difference = Math.abs(currentLevel - nextLevel)
+
+        if (difference < 1 || difference > 3) {
+            return i + 1
+        }
+
+        const direction = currentLevel < nextLevel ? 1 : -1
+
+        if (reportDirection === 0) {
+            reportDirection = direction
+        } else if (reportDirection !== direction) {
+            return i + 1
+        }
+    }
+
+    return null
+}
+
 const part1 = input => {
     let validCount = 0
 
     input.forEach(line => {
         const levels = line.split(/\s+/).map(Number)
-        let valid = true
-        let reportDirection = 0
 
-        while (valid && levels.length > 1) {
-            let currentLevel = levels.shift()
-            const nextLevel = levels[0]
-            const difference = Math.abs(currentLevel - nextLevel)
-
-            if (difference < 1 || difference > 3) {
-                valid = false
-                break
-            }
-
-            const direction = currentLevel < nextLevel ? 1 : -1
-
-            if (reportDirection === 0) {
-                reportDirection = direction
-            } else if (reportDirection !== direction) {
-                valid = false
-                break
-            }
-        }
-
-        if (valid) {
+        if (invalidReportLevel(levels) === null) {
             validCount += 1
         }
     })
@@ -34,6 +37,33 @@ const part1 = input => {
     return validCount
 }
 
+const part2 = input => {
+    let validCount = 0
+
+    input.forEach(line => {
+        const levels = line.split(/\s+/).map(Number)
+        const invalidLevel = invalidReportLevel(levels)
+
+        if (invalidLevel === null) {
+            validCount += 1
+        } else {
+            // Try removing each level in turn
+            for (let i = 0; i < levels.length; i++) {
+                const fixedLevels = [...levels]
+                fixedLevels.splice(i, 1)
+
+                if (invalidReportLevel(fixedLevels) === null) {
+                    validCount += 1
+                    break
+                }
+            }
+        }
+    })
+
+    return validCount
+}
+
 module.exports = {
-    part1
+    part1,
+    part2
 }

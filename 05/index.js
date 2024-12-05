2df943fc55fddffb9986aeed80f750bc4c77dace
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-12-05 21:06:12 +0000

    Day 05 part 2

--- 05/index.js
+++ 05/index.js
@@ -26,6 +26,40 @@ const part1 = input => {
     return total
 }
 
+const part2 = input => {
+    const rules = input.filter(line => line.includes('|'))
+    const updates = input.filter(line => line.includes(','))
+
+    const validate = (pages) => {
+        for (let i = 1; i < pages.length; i++) {
+            for (let j = i; j < pages.length; j++) {
+                if (rules.includes(`${pages[j]}|${pages[i - 1]}`)) {
+                    // Move second one before first
+                    const toMove = pages.splice(j, 1)
+                    pages.splice(i - 1, 0, toMove[0])
+
+                    return validate(pages)
+                }
+            }
+        }
+
+        return pages
+    }
+
+    let total = 0
+
+    for (let update of updates) {
+        let pages = validate(update.split(',').map(Number))
+
+        if (pages.join(',') !== update) {
+            total += pages[Math.floor(pages.length / 2)]
+        }
+    }
+
+    return total
+}
+
 module.exports = {
-    part1
+    part1,
+    part2
 }

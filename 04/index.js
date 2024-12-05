b340f66895594f1ecda402bfffa41767d558b576
Fred Marecesche <fred.marecesche@digital.justice.gov.uk>
2024-12-05 17:08:12 +0000

    Day 04 part 1

---
+++ 04/index.js
@@ -0,0 +1,51 @@
+const part1 = input => {
+    const maxY = input.length -1
+    const maxX = input[0].length -1
+    const word = 'XMAS'
+
+    const directions = {
+        N: { x: 0, y: -1 },
+        NE: { x: 1, y: -1 },
+        E: { x: 1, y: 0 },
+        SE: { x: 1, y: 1 },
+        S: { x: 0, y: 1 },
+        SW: { x: -1, y: 1 },
+        W: { x: -1, y: 0 },
+        NW: { x: -1, y: -1 }
+    }
+
+    let count = 0
+    
+    for (let y = 0; y < input.length; y++) {
+        for (let x = 0; x < input[y].length; x++) {
+            if (input[y][x] === 'X') {
+                for (let direction in directions) {
+                    const endX = directions[direction].x * (word.length - 1) + x
+                    const endY = directions[direction].y * (word.length - 1) + y
+
+                    if (endX >= 0 && endX <= maxX && endY >= 0 && endY <= maxY) {
+                        let startX = x
+                        let startY = y
+
+                        if (word.split('').every(letter => {
+                            const valid = input[startY][startX] === letter
+
+                            startX += directions[direction].x
+                            startY += directions[direction].y
+
+                            return valid
+                        })) {
+                            count += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return count
+}
+
+module.exports = {
+    part1
+}
